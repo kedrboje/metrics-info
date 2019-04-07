7f4df3803066aaf1b32d08fda8d543cb9516c389
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-07 19:53:16 +0300

    added storage for metics and sort feature

--- server.py
+++ server.py
@@ -3,19 +3,40 @@ import asyncio
 
 def run_server(host, port):
 
-    data = {}
-
-    pass
+    local_data = {}
+    sorted_data = {}
 
     async def handle_echo(reader, writer):
         tmp_data = await reader.read(1024)
-        await asyncio.sleep(1)
+        await asyncio.sleep(1) # may be deleted
         message = tmp_data.decode()
-        print(message)
-
-        writer.write(b'ok\n\n')
-        await writer.drain()     
+        
+        if message[:3] == 'put':
+            data = message[:-1].split(' ')[1:]
+            if data[0] not in local_data:
+                local_data[data[0]] = [(data[1], data[2])]
+            else:
+                local_data[data[0]].append((data[1], data[2]))
+            writer.write(b'ok\n\n')
+            await writer.drain() 
+        elif message[:3] == 'get':
+            key = message.split(" ")[1]
+            for item in local_data:
+                tmp_list = sorted(local_data[item], key=lambda x: x[1])
+                sorted_data[item] = tmp_list
+            print(sorted_data)
+            if key == "*":
+                pass
+            elif key in local_data:
+                pass
+            else:
+                writer.write(b'ok\n\n')
+                await writer.drain() 
+        else:
+            writer.write(b'error\nwrong command\n\n')
+            await writer.drain() 
         writer.close()
+        # print(local_data) # should be deleted
 
     loop = asyncio.get_event_loop()
     coro = asyncio.start_server(handle_echo, host=host, port=port, loop=loop)
@@ -30,5 +51,5 @@ def run_server(host, port):
     loop.run_until_complete(server.wait_closed())
     loop.close()
 
-if __name__ == "__main__":
+if __name__ == "__main__": # should be deleted
     run_server('127.0.0.1', 8181)
