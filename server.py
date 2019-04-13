ac725ec3d44cbe66bd0e228bc55ebf628353a6d6
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-13 19:03:08 +0300

    added cheking for data and while-True loop

--- server.py
+++ server.py
@@ -7,34 +7,41 @@ def run_server(host, port):
     sorted_data = {}
 
     async def handle_echo(reader, writer):
-        tmp_data = await reader.read(1024)
-        await asyncio.sleep(1) # may be deleted
-        message = tmp_data.decode()
-        
-        if message[:3] == 'put':
-            data = message[:-1].split(' ')[1:]
-            if data[0] not in local_data:
-                local_data[data[0]] = [(int(data[2]), float(data[1]))]
-            else:
-                local_data[data[0]].append((int(data[2]), float(data[1])))
-            writer.write(b'ok\n\n')
-            await writer.drain() 
-        elif message[:3] == 'get':
-            key = message.split(" ")[1]
-            for item in local_data:
-                tmp_list = sorted(local_data[item], key=lambda x: x[0])
-                sorted_data[item] = tmp_list
-            print(sorted_data)
-            if key == "*":
-                pass
-            elif key in local_data:
-                pass
-            else:
-                writer.write(b'ok\n\n')
+        while True:
+            tmp_data = await reader.readline()
+            await asyncio.sleep(1) # may be deleted
+            if tmp_data:
+                message = tmp_data.decode('utf8')
+                print(message)
+                if message[:3] == 'put':
+                    data = message[:-1].split(' ')[1:]
+                    if data[0] not in local_data:
+                        local_data[data[0]] = [(int(data[2]), float(data[1]))]
+                    else:
+                        local_data[data[0]].append((int(data[2]), float(data[1])))
+                    writer.write(b'ok\n\n')
+                    await writer.drain()
+                elif message[:3] == 'get':
+                    key = message.split(" ")[1]
+                    print(key)
+                    for item in local_data:
+                        tmp_list = sorted(local_data[item], key=lambda x: x[0])
+                        sorted_data[item] = tmp_list
+                    print(sorted_data)
+                    if key == "*":
+                        writer.write(b'da\n')
+                    elif key in local_data:
+                        pass
+                    else:
+                        writer.write(b'ok\n\n')
+                        await writer.drain() 
+                        pass
+                else:
+                    writer.write(b'error\nwrong command\n\n')
                 await writer.drain() 
-        else:
-            writer.write(b'error\nwrong command\n\n')
-            await writer.drain() 
+            else:
+                print('no data')
+                break
         writer.close()
         # print(local_data) # should be deleted
 
